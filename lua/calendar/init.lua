d29c535e42aa0c19ef91ae9425b5636eabe6f384
wang <wangminhong73@gmail.com>
2024-12-25 18:00:48 +0800

    Add ui menu popup

--- lua/calendar/init.lua
+++ lua/calendar/init.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+local popup = require("plenary.popup")
+
+local Win_id
+
 local augroup = vim.api.nvim_create_augroup("calendar", { clear = true })
 
 local main = function()
@@ -7,26 +11,43 @@ local main = function()
     return 5
 end
 
-local create_buf = function()
-    local buf = vim.api.nvim_create_buf(true, true)
-    vim.api.nvim_buf_set_name(buf, "*calendar*")
-    vim.api.nvim_set_option_value("filetype", "lua", { buf = buf })
-    return buf
-end
-
-local buf = create_buf()
-
---[[
-vim.api.nvim_buf_set_lines(buf, 0, -1, true, { "-- Welcome to Neovim!", "" })
-vim.api.nvim_win_set_buf(0, buf)
-vim.api.nvim_win_set_cursor(0, { vim.api.nvim_buf_line_count(buf), 0 })
---]]
-
 M.setup = function()
     vim.api.nvim_create_autocmd("VimEnter",
         { group = augroup, once = true, callback = main }
     )
 end
 
+CloseMenu = function()
+    vim.api.nvim_win_close(Win_id, true)
+end
+
+local ShowMenu = function(opts, cb)
+    local parent_win = vim.api.nvim_get_current_win()
+    local width = math.floor(vim.api.nvim_win_get_width(parent_win) * 0.9)
+    local height = math.floor(vim.api.nvim_win_get_height(parent_win) * 0.9)
+    local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+
+    Win_id = popup.create(opts, {
+        title = "Calendar",
+        highlight = "CalendarHighlight",
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        minwidth = width,
+        minheight = height,
+        borderchars = borderchars,
+        callback = cb,
+    })
+    local bufnr = vim.api.nvim_win_get_buf(Win_id)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<cmd>lua CloseMenu()<CR>", {
+        silent = false })
+end
+
+M.toggle_on = function()
+    local opts = {}
+    local cb = function(_, sel)
+        print("works!")
+    end
+    ShowMenu(opts, cb)
+end
 
 return M
